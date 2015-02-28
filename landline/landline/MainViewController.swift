277037ac8898f41872f2bb8ef2b52d4b87b7980b
Jason Lagaac <jason@safetyscissors.co>
2015-02-28 19:15:55 +1100

    Added navigation view controller embed

--- landline/landline/MainViewController.swift
+++ landline/landline/MainViewController.swift
@@ -14,22 +14,27 @@ class MainViewController : UIViewController {
     
     var menuViewController : MenuViewController? = MenuViewController()
     
-    var chatViewController : ChatViewController? = ChatViewController()
+    var centralViewController : UINavigationController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadCentralViewController()
         
-        self.navigationController?.navigationBarHidden = false
+        self.navigationController?.navigationBarHidden = true
         self.navigationItem.hidesBackButton = true
         
         self.menuViewController?.view.frame = self.view.frame;
-        self.chatViewController?.view.frame = self.view.frame;
-        
-        self.slideMenuController = SlideMenuController(mainViewController: chatViewController!, leftMenuViewController: menuViewController!)
+        self.slideMenuController = SlideMenuController(mainViewController: centralViewController!, leftMenuViewController: menuViewController!)
         
         self.addChildViewController(self.slideMenuController!)
         self.slideMenuController?.view.frame = self.view.frame
         self.view.addSubview(self.slideMenuController!.view!)
+    }
+    
+    func loadCentralViewController() {
+        var storyboard = UIStoryboard(name: "Main", bundle: nil)
+        self.centralViewController = storyboard.instantiateViewControllerWithIdentifier("CentralViewController") as?UINavigationController
+        self.centralViewController?.view.frame = self.view.frame;
     }
     
     @IBAction func showMenu() {
